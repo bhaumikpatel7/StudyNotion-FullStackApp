46ac32b7bbdcfaa38a93894377ecaa85427b6818
bhaumikpatel7 <72443943+bhaumikpatel7@users.noreply.github.com>
2024-02-15 10:39:17 -0500

    models completed

---
+++ server/utils/mailSender.js
@@ -0,0 +1,28 @@
+const nodemailder = require("nodemailer");
+require("dotenv").config();
+
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailder.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: "StudyNotion || Bhaumik Patel",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+
+    console.log(info);
+    return info;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = mailSender;
